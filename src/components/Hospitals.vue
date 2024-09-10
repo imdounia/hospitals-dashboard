7f038109d4999a69c45c9d9708d321d3af3d9758
imdounia <douniabakhkhouch2000@gmail.com>
2024-09-10 02:16:53 +0200

    add graph for monthy hospitalization

--- src/components/Hospitals.vue
+++ src/components/Hospitals.vue
@@ -13,9 +13,10 @@
       <div class="tab-pane fade" :class="{ show: currentTab === index, active: currentTab === index }" 
            v-for="(hospital, index) in hospitals" :key="'content-' + hospital.name">
         <div class="custom-card card">
-          <h2 class="card-header">
+          <div class="container my-4">
+          <div class="card-header fs-2 pb-0">
             {{ hospital.name }}
-          </h2>
+          </div>
           <div class="card-body">
               <div class="d-flex align-items-center card-title">      
                   <span class="material-symbols-outlined fs-6 me-1">
@@ -23,44 +24,37 @@
                     </span>
                     <div>{{ hospital.location }}</div>
                 </div>
-
-            <p class="card-text">Total Patients: {{ hospital.overview.totalPatients }}</p>
-            <p class="card-text">Satisfaction Rate: {{ hospital.overview.satisfactionRate }}</p>
           </div>
-          <div class="container my-4">
-          <div class="row">
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-              <div class="card p-3">
-                <h5 class="card-title">Total Patients</h5>
-                <p class="card-text">{{ hospital.overview.totalPatients }}</p>
-              </div>
+          
+          <div class="row justify-content-center">
+            <div class="hospital-overview p-2 col-12 col-md-6 col-lg-2 m-3 d-flex align-items-center flex-column justify-content-between">
+              <h5 class="card-title align-self-start fs-6">Total Patients</h5>
+              <p class="card-text align-self-end fs-2">{{ hospital.overview.totalPatients }}</p> 
             </div>
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-              <div class="card p-3">
-                <h5 class="card-title">Satisfaction Rate</h5>
-                <p class="card-text">{{ hospital.overview.satisfactionRate }}</p>
-              </div>
+            <div class="hospital-overview p-2 col-12 col-md-6 col-lg-2 m-3 d-flex align-items-center flex-column justify-content-between">
+              <h5 class="card-title align-self-start fs-6">Satisfaction Rate</h5>
+              <p class="card-text align-self-end fs-2">{{ hospital.overview.satisfactionRate }}</p>
             </div>
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-              <div class="card p-3">
-                <h5 class="card-title">Total Treatments</h5>
-                <p class="card-text">{{ hospital.overview.totalTreatments }}</p>
-              </div>
+            <div class="hospital-overview p-2 col-12 col-md-6 col-lg-2 m-3 d-flex align-items-center flex-column justify-content-between">
+              <h5 class="card-title align-self-start fs-6">Total Treatments</h5>
+              <p class="card-text align-self-end fs-2">{{ hospital.overview.totalTreatments }}</p>
             </div>
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-              <div class="card p-3">
-                <h5 class="card-title">Number of Doctors</h5>
-                <p class="card-text">{{ hospital.overview.numberOfDoctors }}</p>
-              </div>
+            <div class="hospital-overview p-2 col-12 col-md-6 col-lg-2 m-3 d-flex align-items-center flex-column justify-content-between">
+              <h5 class="card-title align-self-start fs-6">Number of Doctors</h5>
+              <p class="card-text align-self-end fs-2">{{ hospital.overview.numberOfDoctors }}</p>
             </div>
-            <div class="col-12 col-md-6 col-lg-4 mb-3">
-              <div class="card p-3">
-                <h5 class="card-title">Number of Nurses</h5>
-                <p class="card-text">{{ hospital.overview.numberOfNurses }}</p>
-              </div>
+            <div class="hospital-overview p-2 col-12 col-md-6 col-lg-2 m-3 d-flex align-items-center flex-column justify-content-between">
+              <h5 class="card-title align-self-start fs-6">Number of Nurses</h5>
+              <p class="card-text align-self-end fs-2">{{ hospital.overview.numberOfNurses }}</p>
             </div>
-          </div>
+          </div> 
+          
+          
         </div>
+        
+        <div class="container my-4">
+            <canvas :id="'hospitalChart-' + index"></canvas>
+          </div>
 
         </div>
       </div>
@@ -71,6 +65,9 @@
 <script>
 import '@/assets/styles.css';
 import hospitalData from '@/data/data_exemple1.JSON';
+import { Chart, BarController, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend } from 'chart.js';
+
+Chart.register(BarController, BarElement, CategoryScale, LinearScale, Title, Tooltip, Legend);
 
 export default {
     name:'hospitals',
@@ -79,19 +76,101 @@ export default {
       hospitals: hospitalData,
       currentTab: 0 
     };
+  },
+  mounted() {
+    this.renderChart(this.hospitals[this.currentTab], this.currentTab);
+  },
+  watch: {
+    currentTab(newTab) {
+      this.$nextTick(() => {
+        this.renderChart(this.hospitals[newTab], newTab);
+      });
+    }
+  },
+  methods: {
+    renderChart(hospital, index) {
+      const ctx = document.getElementById(`hospitalChart-${index}`).getContext('2d');
+      const labels = hospital.monthlyHospitalizations.map(item => `${item.month} ${item.year}`);
+      const data = hospital.monthlyHospitalizations.map(item => item.value);
+
+      const gradient = ctx.createLinearGradient(0, 0, ctx.canvas.width, 0);
+      gradient.addColorStop(0, '#CF55F4');
+      gradient.addColorStop(1, '#7E3BFA');
+
+      if (this.chart) {
+        this.chart.destroy();
+      }
+
+      this.chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Monthly Hospitalizations',
+              data: data,
+              backgroundColor: gradient,
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          },
+          plugins: {
+            legend: {
+              display: true
+            },
+            title: {
+              display: true,
+              text: `Monthly Hospitalizations - ${hospital.name}`
+            }
+          }
+        }
+      });
+    }
   }
 }
 </script>
 
 <style scoped>
+/* canvas {
+  max-width: 100%;
+  max-height: 2000px;
+} */
+/* @media (max-width: 600px) {
+  canvas {
+    max-height: 1500px; 
+  }
+} */
+
 .custom-card {
   background: var(--background-card);
   color: var(--text-color);
 }
 
-.card-title {
-  color: #B18CFE;
+.hospital-overview {
+  
+  height: 7rem;
+  /* padding: 20px;
+  border: 1px solid #ccc; */
 }
+
+.hospital-overview:nth-child(odd) {
+  color: var(--primary-color);
+  background-color: #2C3048;
+}
+.hospital-overview:nth-child(even) {
+  color: var(--secondary-color);
+  background-color: var(--dark-blue);
+}
+
+/* .card-title {
+  color: #B18CFE;
+} */
 
 /* .button-primary {
   background-color: #EE7DFF;
--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import '@/assets/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'chart.js';
 
 createApp(App).mount('#app')
